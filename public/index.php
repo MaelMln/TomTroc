d662c7d0543e460e6fe9ea10af1db9236068086d
mael <moulianmael@gmail.com>
2024-10-21 16:08:28 +0200

    21/10 changes v2

--- public/index.php
+++ public/index.php
@@ -4,13 +4,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 define('ROOT_DIR', dirname(__DIR__));
 
 require_once __DIR__ . '/../src/Util/Autoloader.php';
-\App\Util\Autoloader::register();
 
+use App\Util\Autoloader;
 use App\Kernel;
+
+Autoloader::register();
 
 $kernel = new Kernel();
 $kernel->handleRequest();
